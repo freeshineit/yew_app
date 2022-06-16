6ff187ce382ef8555473ca6f42327595cf9c756f
freeshineit <xiaoshaoqq@gmail.com>
2022-06-16 19:03:00 +0800

    feat: layout

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,13 +1,35 @@
+use crate::components::atoms::layout::{Layout, LayoutHeader};
 use crate::Route;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
+use stylist::style;
+
 #[function_component(Home)]
 pub fn home() -> Html {
-  html! {
-    <div>
-      <h1>{"Home"}</h1>
-      <Link<Route> to={Route::TodoList}>{"todo list"}</Link<Route>>
-    </div>
-  }
+    // let style = style!(
+    //     r#"
+    //     background-color: red;
+
+    //     .nested {
+    //         background-color: blue;
+    //         width: 100px
+    //     }
+    // "#
+    // )
+    // .expect("Failed to mount style!");
+
+    html! {
+      <Layout class={String::from("adfadsfa")}>
+        <LayoutHeader />
+          <div>
+          <Link<Route> to={Route::TodoList}>{"todo list"}</Link<Route>>
+          // <Button />
+          </div>
+          <div>
+            <h1>{"Home"}</h1>
+            <img src="/logo.png" />
+          </div>
+      </Layout>
+    }
 }
