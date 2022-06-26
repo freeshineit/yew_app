bbfe854a7f3b7af8e2844c928e969c00cea936e4
ShineShao <xiaoshaoqq@gmail.com>
2022-06-27 01:11:09 +0800

    style: layout

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,4 +1,3 @@
-use crate::components::atoms::layout::{Layout, LayoutHeader};
 use crate::Route;
 use yew::prelude::*;
 use yew_router::prelude::*;
@@ -7,29 +6,34 @@ use stylist::style;
 
 #[function_component(Home)]
 pub fn home() -> Html {
-    // let style = style!(
-    //     r#"
-    //     background-color: red;
+    let style = style!(
+        r#"
+        text-align: center;
+        flex: 1 1 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 40px;
 
-    //     .nested {
-    //         background-color: blue;
-    //         width: 100px
-    //     }
-    // "#
-    // )
-    // .expect("Failed to mount style!");
+        a {
+          margin: 0 10px;
+        }
+
+        .nested {
+            background-color: blue;
+            width: 100px
+        }
+    "#
+    )
+    .expect("Failed to mount style!");
 
     html! {
-      <Layout class={String::from("adfadsfa")}>
-        <LayoutHeader />
+      <div class={style}>
+          <img src="/assets/logo.png"  />
           <div>
-          <Link<Route> to={Route::TodoList}>{"todo list"}</Link<Route>>
-          // <Button />
+            <Link<Route> to={Route::Home}>{"Home"}</Link<Route>>
+            <Link<Route> to={Route::TodoList}>{"Todo List"}</Link<Route>>
           </div>
-          <div>
-            <h1>{"Home"}</h1>
-            <img src="/assets/filter.png"  />
-          </div>
-      </Layout>
+      </div>
     }
 }
