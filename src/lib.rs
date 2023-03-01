64a4627cb7fe5089685020ad11dceddc981121a5
ShineShao <xiaoshaoqq@gmail.com>
2023-03-01 22:53:55 +0800

    add wee_alloc and optimize size

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,12 @@ use yew_router::prelude::*;
 
 use crate::components::atoms::layout::{Layout, LayoutFooter, LayoutHeader};
 
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 // root route
 #[derive(Debug, Clone, Copy, PartialEq, Routable)]
 pub enum Route {
